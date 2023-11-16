4447d7db64044b204d507b12fb83cecbd80b307b
Christopher Bannon <bannon.c.35@berkeley.edu>
2023-11-15 23:06:05 -0800

    P much finished impl, working on menu rn

--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -7,9 +7,10 @@
 	export let fontChanges: number;
 	export let showMenu: boolean;
 	export let alignment: string;
+	let menu: number = 0;
 
 	let mixnmatch: boolean = false;
-	import { fonts, font } from '../stores';
+	import { fonts, font, ascii_struct } from '../stores';
 	import { updateAscii } from './keyboardlogic';
 
 	async function update() {
@@ -18,16 +19,31 @@
 	}
 </script>
 
-<div style="--bg: {backgroundColor}; --color: {color}">
-	<div class="menu" style="display: {showMenu ? '' : 'none'}">
-		<input type="color" bind:value={backgroundColor} id="backgroundColor" />
-		<input type="color" bind:value={color} id="color" />
+<div class="menuOverlay" style="display: {showMenu ? '' : 'none'}">
+	<div class="menu">
+		<div class="titleBar">
+			<h1>OPTIONS</h1>
+			<button
+				class="menuButtonInverse"
+				on:click={() => {
+					showMenu = false;
+				}}>x</button
+			>
+		</div>
+
+		<div class="menuContents">
+			<p class="item-l">BACKGROUND:</p>
+			<input class="item-r" type="color" bind:value={backgroundColor} id="backgroundColor" />
+			<p class="item-l">COLOR:</p>
+			<input class="item-r" type="color" bind:value={color} id="color" />
+		</div>
+
 		<button
 			on:click={() => {
 				mode = update_color_mode(mode);
 			}}>change mode</button
 		>
-		<select
+		<!-- <select
 			class="fonts"
 			bind:value={$font}
 			on:change={() => {
@@ -56,30 +72,76 @@
 				on:click={() => {
 					alignment = 'center';
 				}}>M</button
-			>
-			<button
+			> -->
+		<!-- <button
 				on:click={() => {
 					alignment = 'flex-end';
 				}}>R</button
 			>
-		</div>
-		<button
-			on:click={() => {
-				showMenu = !showMenu;
-			}}>Hide Menu</button
-		>
+		</div> -->
 	</div>
-	<div class="menuItem" style="--bg: {backgroundColor}" />
+</div>
+<div class="menuBar" style="--bg: {backgroundColor}">
+	<button class="menuButton">&uarr;</button>
+	<h1 class="title">ASCII SANDBOX</h1>
+	<button
+		class="menuButton"
+		on:click={() => {
+			showMenu = true;
+		}}>SETTINGS</button
+	>
 </div>
 
 <style lang="postcss">
-	.menu {
-		@apply h-[100px] w-screen flex flex-row  items-center justify-around;
+	.menuOverlay {
+		@apply h-screen w-screen fixed top-0 left-0 flex justify-center items-center z-10;
 	}
-	.menuItem {
-		@apply bg-[var(--color)] text-[var(--bg)];
+	.menu {
+		@apply h-5/6 w-9/12 border-4  border-[var(--color)] p-4 flex flex-col bg-[var(--bg)] text-[var(--color)];
+	}
+	.menu h1 {
+		@apply text-4xl border-b-4 border-[var(--color)];
+	}
+	.exit {
+		@apply text-4xl;
+	}
+	.title {
+		@apply text-4xl;
+		@media (max-width: 600px) {
+			display: none;
+		}
+	}
+	.titleBar {
+		@apply flex flex-row justify-between text-3xl p-2 sticky top-0 mb-4;
+	}
+	.menuBar {
+		@apply bg-[var(--color)] text-[var(--bg)] flex flex-row justify-between text-2xl p-2 top-0 w-screen sticky left-0;
 	}
 	.fonts {
-		@apply h-min text-[var(--bg)];
+		@apply h-min text-[var(--color)];
 	}
+	.menuButton {
+		@apply px-2 border-4 border-[var(--color)] hover:border-[var(--bg)] active:bg-[var(--bg)] active:text-[var(--color)];
+	}
+	.menuButtonInverse {
+		@apply px-2 border-4 border-[var(--bg)] hover:border-[var(--color)] active:bg-[var(--color)] active:text-[var(--bg)];
+	}
+	.menuContents {
+		@apply grid gap-4 p-2 grid-cols-4;
+	}
+	.menuContents p {
+		@apply text-xl p-1 bg-[var(--color)] text-[var(--bg)];
+	}
+	.item-r {
+		@apply col-start-2;
+	}
+	.item-l {
+		@apply col-start-1;
+	}
+	/* .options {
+		@apply flex flex-col gap-4 w-1/2 border-4 border-[var(--color)];
+	}
+	.fonts {
+		@apply flex flex-col gap-4 w-1/2 border-4 border-[var(--color)];
+	} */
 </style>
--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,14 @@ export function update_color_mode(mode: ColorMode) {
 			return ColorMode.GLOBAL;
 	}
 }
+
+export interface ascii {
+	str: string;
+	encoding: string;
+	color: string;
+}
+
+export interface ascii_row {
+	ascii: ascii[];
+	color: string;
+}
